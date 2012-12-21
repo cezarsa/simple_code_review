bc00694564488c2e7a523a110488e78e1a9bfc93
Davi Ferreira <davi.ferreira@corp.globo.com>
2012-12-21 14:03:24 -0200

    Compact version of the home page

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
 (function ($) {
-    var height = 0,
-        panels = $('#repositories').find('.panel');
-    panels.each(function () {
-        if ($(this).outerHeight() > height) {
-            height = $(this).outerHeight();
-        }
-    }).height(height);
-
     $('textarea').elastic();
 })(jQuery);
